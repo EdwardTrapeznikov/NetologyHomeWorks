b7ee2d92468286afb5801d0cb1dd20f85c6df67d
EdwardTrapeznikov <edwarddd@inbox.ru>
2025-03-20 15:59:38 +0300

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+class Calkulator {
+    public static void main (String[] args) {
+        int income = 35000;
+        int spending = 15000;
+        String text = "Итого (руб):";
+        System.out.println(text);
+        System.out.println(income - spending);
+    }
+}
